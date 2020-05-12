258384889c057ffa4fa324b7d02cfe60608abbdc
OlgaDanilovaV <danilovaolga2235@gmail.com>
2020-05-12 18:17:01 +0300

    Changed BmiService

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int cost) {
-        int weight = 60;
+    public int calculate(int weight) {
+        int mass = 60;
         double height = 1.71;
         double index = weight / (height * height);
         return (int) index;
